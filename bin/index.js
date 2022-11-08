03136aa227667b9e024dfda4fa4a16f18aac1d68
khalilrached <khalilrached058@gmail.com>
2022-11-08 14:48:57 +0100

    code generating

---
+++ bin/index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const yargs = require('yargs/yargs');
+const {hideBin} = require('yargs/helpers');
+
+yargs(hideBin(process.argv))
+  .command('serve [port]', 'start the server', (yargs) => {
+    return yargs
+      .positional('port', {
+        describe: 'port to bind on',
+        default: 5000
+      })
+  }, (argv) => {
+    if (argv.verbose) console.info(`start server on :${argv.port}`)
+    const gh = require('../lib/ModelParser');
+    const test = new gh();
+    console.log(test.generate())
+  })
+  .option('verbose', {
+    alias: 'v',
+    type: 'boolean',
+    description: 'Run with verbose logging'
+  })
+  .help()
+  .parse()
